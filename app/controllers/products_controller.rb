1e28b65ee89ced3af9fa4645fdca2fcdf84a27d0
suginotaisei629 <sugino.t29@gmail.com>
2025-04-22 12:21:02 +0900

    カリキュラム１６番

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -47,6 +47,6 @@ class ProductsController < ApplicationController
     end
 
     def product_params
-      params.require(:product).permit(:name, :description, :featured_image) 
+      params.require(:product).permit(:name, :description, :featured_image, :inventory_count) 
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
-  resources :products
+
+  resources :products do
+    resources :subscribers, only: [ :create ]
+  end
+
+  resource :unsubscribe, only: [:show]
+  
   resource :session, only: [:new, :create, :destroy]
   resources :passwords, only: [:new, :create, :edit, :update] 
   root "products#index"
